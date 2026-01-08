54b42c4d5aaa3fe55e7859ea76def28948b32733
Alastair Patrick <alastairpatrick@gmail.com>
2026-01-08 08:00:03 -0500

    Remove hard coded PDF data.

--- viewmodel/bin_range_viewmodel.py
+++ viewmodel/bin_range_viewmodel.py
@@ -6,19 +6,9 @@ from concurrent.futures import Future
 
 from viewmodel.background_task import BackgroundTask
 
-# Try to use reportlab if available for nicer PDF generation; fall back to
-# the built-in minimal generator when reportlab isn't installed.
-#try:
-#    from reportlab.pdfgen import canvas
-#    from reportlab.lib.pagesizes import letter
-#    _HAVE_REPORTLAB = True
-#except Exception:
-#    print("reportlab not available; using minimal PDF generator")
-#    _HAVE_REPORTLAB = False
-
-_HAVE_REPORTLAB = True
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+
 
 class BinRangeViewModel:
     def __init__(self, executor, name: str = "Bin Range Report", delay: float = 1.0):
@@ -38,7 +28,7 @@ class BinRangeViewModel:
             if self._delay and self._delay > 0:
                 time.sleep(self._delay)
             progress("Assembling PDF bytes")
-            # simple hard-coded minimal PDF bytes containing "Hello World"
+            # generate PDF bytes using ReportLab
             pdf = _make_hello_pdf_bytes()
             return pdf
 
@@ -46,55 +36,11 @@ class BinRangeViewModel:
 
 
 def _make_hello_pdf_bytes() -> bytes:
-    if _HAVE_REPORTLAB:
-        buf = io.BytesIO()
-        c = canvas.Canvas(buf, pagesize=letter)
-        c.setFont("Helvetica", 24)
-        # place "Hello, World" near the top-left with some margin
-        c.drawString(72, 720, "Hello, World")
-        c.showPage()
-        c.save()
-        return buf.getvalue()
-
-    # Fallback: Build a minimal PDF programmatically with correct offsets
-    parts = []
-    offsets = []
-
-    def add(s: bytes):
-        offsets.append(len(b"".join(parts)))
-        parts.append(s)
-
-    header = b"%PDF-1.4\n%\xFF\xFF\xFF\xFF\n"
-    add(header)
-
-    obj1 = b"1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n"
-    add(obj1)
-
-    obj2 = b"2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n"
-    add(obj2)
-
-    obj3 = b"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n"
-    add(obj3)
-
-    stream = b"BT /F1 24 Tf 72 720 Td (Hello World) Tj ET"
-    obj4_stream = b"4 0 obj\n<< /Length %d >>\nstream\n" % (len(stream),)
-    add(obj4_stream)
-    add(stream + b"\n")
-    obj4_end = b"endstream\nendobj\n"
-    add(obj4_end)
-
-    obj5 = b"5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n"
-    add(obj5)
-
-    # xref
-    xref_start = len(b"".join(parts))
-    xref = [b"xref\n0 6\n0000000000 65535 f \n"]
-    for off in offsets:
-        xref.append(b"%010d 00000 n \n" % (off,))
-    xref_bytes = b"".join(xref)
-    parts.append(xref_bytes)
-
-    trailer = b"trailer\n<< /Root 1 0 R /Size 6 >>\nstartxref\n%d\n%%EOF\n" % (xref_start,)
-    parts.append(trailer)
-
-    return b"".join(parts)
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter)
+    c.setFont("Helvetica", 24)
+    # place "Hello, World" near the top-left with some margin
+    c.drawString(72, 720, "Hello, World")
+    c.showPage()
+    c.save()
+    return buf.getvalue()
